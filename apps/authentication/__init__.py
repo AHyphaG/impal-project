cbd06973a31d4eabff1d31a29f46f047f190c5cc
Alfin Abrar Putra <alfinabrarputra@gmail.com>
2024-11-29 19:19:46 +0700

    Initial pengaturan alamat pada register

--- apps/authentication/__init__.py
+++ apps/authentication/__init__.py
@@ -5,7 +5,6 @@ Copyright (c) 2019 - present AppSeed.us
 
 from flask import Blueprint
 from .models import Users
-from .Alamat import Alamat
 from .Vehicles import Vehicles
 
 blueprint = Blueprint(
